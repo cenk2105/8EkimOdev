2959f6d89a10d330105cd4f71ef7671895e0a2e0
ccenk <ccenk@DESKTOP-UNFJT7J>
2024-10-16 13:04:27 +0300

    Proje dosyası ekle.

---
+++ 8EkimOdev/Ogrenci.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8EkimOdev
+{
+    public class Ogrenci
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soyisim;
+        private double vize;
+        private double final;
+        private string okul;
+
+        public Ogrenci(int ogrenciNo,string isim,string soyisim,double vize,double final,string okul)
+        {
+            this.ogrenciNo = ogrenciNo;
+            this.isim = isim;
+            this.soyisim = soyisim;
+            this.vize = vize;
+            this.final = final;
+            this.okul = okul;
+        }
+
+        public void ogrenciBilgiGoster()
+        {
+            Console.WriteLine($"Numara : {this.ogrenciNo} \nIsim : {this.isim}\n" +
+                              $"Soyisim : {this.soyisim}\nVize : {this.vize}\n" +
+                              $"Final : {this.final}\nOkul : {this.okul}");
+        }
+
+        public void ogrenciOrtalamasiBul()
+        {
+            double ortalama = this.vize * 0.4 + this.final * 0.6;
+            Console.WriteLine("{0} ogrencinin ortalamasi : {1}",this.isim,ortalama);
+        }
+        public string okulGetir()
+        {
+            return this.okul;
+        }
+        
+
+        
+        
+
+
+    }
+}
---
+++ 8EkimOdev/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8EkimOdev
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci(1234, "Cenk", "Koc", 90, 100, "Karabuk");            
+            int islem = 0;
+
+            Console.WriteLine("1: Bilgi Goster , 2: Ortalama Goster, 3: Okul Goster , 4: Cikis Yap");
+            try { 
+                islem = Convert.ToInt16(Console.ReadLine());
+            }
+            catch ( Exception e) {  Console.WriteLine(e.Message); }
+
+           
+            switch (islem)
+            {
+                case 1:
+                    ogrenci1.ogrenciBilgiGoster();
+                    break;
+                case 2:
+                    ogrenci1.ogrenciOrtalamasiBul();
+                    break;
+                case 3:
+                    Console.WriteLine(ogrenci1.okulGetir());
+                    break;
+                case 4:
+                    Console.WriteLine("Cikis yapildi !!!");
+                    break;
+                default:
+                    Console.WriteLine("Secim yapmadiniz !!!");
+                    break;
+            }
+        
+            Console.ReadLine();
+
+        }
+    }
+}
